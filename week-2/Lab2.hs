4012c79c799af782512606de9d652c6141250ff3
Ger <gerrit.krijnen@hotmail.com>
2015-09-08 21:23:40 +0200

    Fixed IBAN assignment

--- week-2/Lab2.hs
+++ week-2/Lab2.hs
@@ -1,3 +1,6 @@
+import Data.Char
+-- Data.Char is imported for isAlphaNum check in IBAN assignment
+
 -- Triangle Assignment
 -- Time taken : 40 minutes
 data Shape = NoTriangle | Equilateral 
@@ -55,3 +58,60 @@ perms [] = [[]]
 perms (x:xs) = concat (map (insrt x) (perms xs)) where
 insrt x [] = [[x]]
 insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)
+
+-- IBAN Assignment
+-- Time Taken:  50 minutes
+
+iban :: String -> Bool
+iban xs = calculateModulo97 (convertLetters (shiftFour (removeNonAlphaNum xs))) == 1
+
+removeNonAlphaNum :: String -> String
+removeNonAlphaNum [] = []
+removeNonAlphaNum (x:xs)
+    | isAlphaNum x =  x : removeNonAlphaNum xs 
+    | otherwise = removeNonAlphaNum xs
+
+shiftFour :: String -> String
+shiftFour (a:b:c:d:xs) = xs ++ [a] ++ [b] ++ [c] ++ [d]
+shiftFour _ = []
+
+convertLetters :: String -> String
+convertLetters [] = []
+convertLetters (x:xs)
+    |  x == 'a' || x == 'A' = concat ["10", (convertLetters xs)]
+    |  x == 'b' || x == 'B' = concat ["11", (convertLetters xs)]
+    |  x == 'c' || x == 'C' = concat ["12", (convertLetters xs)]
+    |  x == 'd' || x == 'D' = concat ["13", (convertLetters xs)]
+    |  x == 'e' || x == 'E' = concat ["14", (convertLetters xs)]
+    |  x == 'f'  || x == 'F' = concat ["15", (convertLetters xs)]
+    |  x == 'g' || x == 'G' = concat ["16", (convertLetters xs)]
+    |  x == 'h' || x == 'H' = concat ["17", (convertLetters xs)]
+    |  x == 'i'  || x == 'I' = concat ["18", (convertLetters xs)]
+    |  x == 'j'  || x == 'J' = concat ["19", (convertLetters xs)]
+    |  x == 'k' || x == 'K' = concat ["20", (convertLetters xs)]
+    |  x == 'l'  || x == 'L' = concat ["21", (convertLetters xs)]
+    |  x == 'm' || x == 'M' = concat ["22", (convertLetters xs)]
+    |  x == 'n' || x == 'N' = concat ["23", (convertLetters xs)]
+    |  x == 'o' || x == 'O' = concat ["24", (convertLetters xs)]
+    |  x == 'p' || x == 'P' = concat ["25", (convertLetters xs)]
+    |  x == 'q' || x == 'Q' = concat ["26", (convertLetters xs)]
+    |  x == 'r' || x == 'R' = concat ["27", (convertLetters xs)]
+    |  x == 's' || x == 'S' = concat ["28", (convertLetters xs)]
+    |  x == 't' || x == 'T' = concat ["29", (convertLetters xs)]
+    |  x == 'u' || x == 'U' = concat ["30", (convertLetters xs)]
+    |  x == 'v' || x == 'V' = concat ["31", (convertLetters xs)]
+    |  x == 'w' || x == 'W' = concat ["32", (convertLetters xs)]
+    |  x == 'x' || x == 'X' = concat ["33", (convertLetters xs)]
+    |  x == 'y' || x == 'Y' = concat ["34", (convertLetters xs)]
+    |  x == 'z' || x == 'Z' = concat ["35", (convertLetters xs)]
+    | otherwise = x : convertLetters xs
+
+calculateModulo97 :: String -> Integer
+calculateModulo97 xs
+    | length xs > 9 = calculateModulo97 (modulo97FirstNine xs)
+    | otherwise = (read xs) `mod` 97
+    
+modulo97FirstNine :: String -> String
+modulo97FirstNine [] = []
+modulo97FirstNine xs = concat [show ((read (take 9 xs)) `mod` 97) , drop 9 xs]
+
