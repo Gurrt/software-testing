d59a6985368158927a60d837effa2cb88aafd7d4
Robert Diebels <robertdiebels@grexx.net>
2015-09-17 22:04:43 +0200

    removing arrows and nnf conversion works. Making orsInward however doesnt yet work

--- week-3/Lab3.hs
+++ week-3/Lab3.hs
@@ -1,5 +1,20 @@
 module Lab3 where
-    
-    import Data.List
-    import System.Random
-    import Lecture3
+
+import Data.List
+import System.Random
+import Lecture3
+
+stringToCNF:: String -> String
+stringToCNF s = (showLst.convertToCNF.parse) s
+
+convertToCNF:: [Form] -> [Form]
+convertToCNF [] = []
+convertToCNF (f:fs) = (nnf.arrowfree) f : convertToCNF fs
+
+--orsInward:: Form -> Form
+--orsInward (Prop x) = Prop x
+--orsInward (Neg (Prop x)) = Neg (Prop x)
+--orsInward (Cnj f1:f2:fs) = Dsj[Cnj[f1', f2'], Cnj[f1', f3']]
+--where f1' = map orsInward f1
+--f2' = map orsInward f2
+--f3' = map orsInward f3
