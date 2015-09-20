356697714dd48e685716f183f4ad92db1aa25af7
CarlaA <camialra11@gmail.com>
2015-09-20 22:33:42 +0200

    Adding test report
    week 3 exercise 2

--- week-3/lab3.hs
+++ week-3/lab3.hs
@@ -23,11 +23,13 @@ equiv f g = tautology (Equiv f g)
 --exercise 2
 
 
+-- Generating random formulas
+
 getRandomInt :: Int -> IO Int 
 getRandomInt n = getStdRandom (randomR (0,n))
 
 getRandomF :: IO Form 
-getRandomF = do d <- getRandomInt 4 
+getRandomF = do d <- getRandomInt 5 
                 getRandomForm d
 
 getRandomForm :: Int -> IO Form 
@@ -62,57 +64,28 @@ getRandomFs n = do d <- getRandomInt 3
 
 getRandomForms :: Int -> Int -> IO [Form] 
 getRandomForms _ 0 = return [] 
-getRandomForms d n = do 
-	f <- getRandomForm d 
-	fs <- getRandomForms d (n-1) 
-	return (f:fs)
+getRandomForms d n = do
+        f <- getRandomForm d 
+        fs <- getRandomForms d (n-1) 
+        return (f:fs)
 
 test :: Int -> (Form -> Bool) -> [Form] -> IO () 
-test n _ [] = print (show n ++ "Valid") 
+test n _ [] = print (show n ++ " Valid") 
 test n p (f:fs) = 
-	if p f 
-	then do print ("pass on:" ++ show f) test n p fs 
-	else error ("fail on:" ++ show f)
+  if p f 
+  then do print ("pass on: " ++ show f) 
+          test n p fs
+  else error ("fail on: " ++ show f)
 
 testForms :: Int -> (Form -> Bool) -> IO () 
-testForms n p = do 
-	fs <- getRandomFs n 
-	test n p fs
+testForms n p = do
+        fs <- getRandomFs n
+        test n p fs
 
-testParser = testForms 100 
+-- Testing parse function
+
+testParserFunc = testForms 50 
 	(\ f -> let [g] = parse (show f) in f == g)
 
-
-
--- exercise 3
-
--- precondition: input forms are in cnf
-dist' :: Form -> Form -> Form
-dist' p (Cnj fs) = Cnj (map (\ x -> dist' p x) fs)
-dist' (Cnj fs) q = Cnj (map (\ x -> dist' x q) fs)
-dist' (Dsj ps) (Dsj qs) = Dsj (ps ++ qs)  {- These three lines prevent unnecessary Dsj nesting -}
-dist' (Dsj ps) q = Dsj (ps ++ [q])
-dist' p (Dsj qs) = Dsj (p : qs)
-dist' p q = Dsj [p, q]
-
--- precondition: all input forms are in cnf
-dist :: [Form] -> Form
-dist [] = error "empty list"
-dist [f] = f
-dist (f : fs) = dist' f (dist fs)
-
--- precondition: input is in nnf
-cnf :: Form -> Form
-cnf (Prop p) = Prop p
-cnf (Neg p) = Neg p
-cnf (Cnj fs) = Cnj (map cnf fs)
-cnf (Dsj fs) = dist (map cnf fs)
-
--- Function that can transform any formula to CNF
-formToCNF :: Form -> Form
-formToCNF = cnf . nnf . arrowfree
--- VVZ: You miss another function that would 'flatten' nested conjunctions and disjunctions. The formulae on the slides used associativity and hence assumed the flattener of x & (y & z) to x & y & z in the head of the reader, but in the implementation your rewritings could make quite a mess of the structure of conjunction/disjunction lists, not to mention that the input is 'any formula', so it can be already messed up.
--- VVZ: formToCNF (Cnj [Cnj [p,q,p], q])
--- VVZ: I expect to see (p AND q AND p AND q), not ((p AND q AND p) AND q)
 
 
---
+++ week-3/testReport.md
@@ -0,0 +1,8 @@
+Test-Report Week 3
+
+--exercise 2
+
+We took a well know function as starting point in order to generate random formulas that generates an integer of n numbers, then it generates a random formula with limit 5 for this case.
+In case of formulas with zero levels, it generates an arbitrary number of indices.
+We make an arrangement for formulas until 5 levels in order to fulfil some syntactic forms and then generate random formulas with variable levels.
+Then we create the test function with 50 random generated formulas showing the ones that pass the parsing test and the ones that do not.
