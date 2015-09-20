5217a384cc398610ca82952cc4c2066f0453243e
Ger <gerrit.krijnen@hotmail.com>
2015-09-20 21:33:18 +0200

    Added documentation for exercise 1, very tired from busy weekend, going to bed!

--- week-3/Lab3.hs
+++ week-3/Lab3.hs
@@ -3,15 +3,28 @@ import Data.List
 import System.Random
 import Lecture3
 
+
+-- Exercise 1
+-- Time: 2 Hours
+
+-- Makes a List of truth values for Form f
 truths :: Form -> [Bool]
 truths f = map (\v -> evl v f) (allVals f)
 
+-- A contradiction is where all truth values are False
+-- Definition is very straightforward, just checked definition with a few short formulas.
 contradiction :: Form -> Bool
 contradiction f = all (\x -> not x) (truths f)
 
+-- A Tautology is where all truth values are True
+-- Definition is straightforward, just checked definition with a few short formulas.
 tautology :: Form -> Bool
 tautology f = all (\x-> x) (truths f)
 
+-- Entailment is where if F is true, G is true too.
+-- Checked definition with a couple tailor made formulas.
+-- Couldn't find information on where entailment can still be achieved with differing number
+-- of variables, so we assumed that it cannot.
 entail :: Form -> Form -> Bool
 entail f g = satisfiable f &&
                  length (truths f) == length (truths g) &&
@@ -23,5 +36,9 @@ isEntailment ((x,y):xs)
      | x && not y = False
      | otherwise = isEntailment xs
 
+-- Equivalence is where the truth table of F equals the truth table of G
+-- Definition is very straightforward and relies heavily on the evl and allVals functions from the lecture, which we assume to be correct
+--  so we just checked definition with a few short formulas.
 equiv :: Form -> Form -> Bool
 equiv f g = truths f == truths g
+
