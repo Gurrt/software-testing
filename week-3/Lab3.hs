529df751976ffb551c19cf799b40d9a9a8acc64b
Robert Diebels <robertdiebels@grexx.net>
2015-09-20 22:12:01 +0200

    Distribution of a disjunction works correctly now.

--- week-3/Lab3.hs
+++ week-3/Lab3.hs
@@ -3,27 +3,43 @@ module Lab3 where
 import Data.List
 import System.Random
 import Lecture3
+--Exercise 2
+--Our goal is to test this function
+
+-- Precondition, string consists of digits,'(',')','*','+','-','==>','<=>'
+--parse :: String -> [Form]
+--parse s = [ f | (f,_) <- parseForm (lexer s) ]
+-- Postcondition, list of Forms corresponding to the used strings
+
+--See Tests.hs
 
 --Exercise 3
 
+--Parse a given string, convert the given Forms to CNF, Show the forms
 stringToCNF:: String -> String
 stringToCNF s = (showLst.convertToCNF.parse) s
 
+--Make a Form in the Form list arrowfree, move negations inward, and distribute Disjunctions
 convertToCNF:: [Form] -> [Form]
 convertToCNF [] = []
 convertToCNF (f:fs) = (distribute.nnf.arrowfree) f : convertToCNF fs
 
+--Negations and prop's can be returned immedately, Conjunction only need the forms within distributed
+-- Perform distrubition specifically for Disjunctions
 distribute:: Form -> Form
 distribute (Prop x) = Prop x
 distribute (Neg (Prop x)) = Neg (Prop x)
 distribute (Cnj fs) = Cnj (map distribute fs)
 distribute (Dsj fs) = distributeDsjFormList (fs)
 
+--Perform disjucntion distribution on all Forms in a Disjuntion
 distributeDsjFormList:: [Form] -> Form
 distributeDsjFormList [] = error "Disjunction on one element?"
 distributeDsjFormList [f] = f
 distributeDsjFormList (f : fs) = distributeDsj' f (distributeDsjFormList fs)
 
+--If either the first or the second argument is a Conjuction apply distribution according to Distributive laws of Disjunctions
+--In any other case distribute the 2 forms and add them to a disjunction.
 distributeDsj':: Form -> Form -> Form
 distributeDsj' f1 (Cnj(f2:f3)) = Cnj [Dsj[f1', f2'] ,Dsj([f1']++f3')] where
                          f1' = distribute f1
