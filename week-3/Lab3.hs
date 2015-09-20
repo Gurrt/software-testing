39493746d55f50bf6c473cf49fe0da3676ab54df
Robert Diebels <robertdiebels@grexx.net>
2015-09-20 17:10:24 +0200

    Distribution of a disjunction works correctly now.

--- week-3/Lab3.hs
+++ week-3/Lab3.hs
@@ -4,17 +4,36 @@ import Data.List
 import System.Random
 import Lecture3
 
+--Exercise 3
+
 stringToCNF:: String -> String
 stringToCNF s = (showLst.convertToCNF.parse) s
 
 convertToCNF:: [Form] -> [Form]
 convertToCNF [] = []
-convertToCNF (f:fs) = (nnf.arrowfree) f : convertToCNF fs
+convertToCNF (f:fs) = (distribute.nnf.arrowfree) f : convertToCNF fs
 
---orsInward:: Form -> Form
---orsInward (Prop x) = Prop x
---orsInward (Neg (Prop x)) = Neg (Prop x)
---orsInward (Cnj f1:f2:fs) = Dsj[Cnj[f1', f2'], Cnj[f1', f3']]
---where f1' = map orsInward f1
---f2' = map orsInward f2
---f3' = map orsInward f3
+distribute:: Form -> Form
+distribute (Prop x) = Prop x
+distribute (Neg (Prop x)) = Neg (Prop x)
+distribute (Cnj fs) = Cnj (map distribute fs)
+distribute (Dsj fs) = distributeDsjFormList (fs)
+
+distributeDsjFormList:: [Form] -> Form
+distributeDsjFormList [] = error "Disjunction on one element?"
+distributeDsjFormList [f] = f
+distributeDsjFormList (f : fs) = distributeDsj' f (distributeDsjFormList fs)
+
+distributeDsj':: Form -> Form -> Form
+distributeDsj' f1 (Cnj(f2:f3)) = Cnj [Dsj[f1', f2'] ,Dsj([f1']++f3')] where
+                         f1' = distribute f1
+                         f2' = distribute f2
+                         f3' = map (\f -> distribute f ) f3
+distributeDsj' (Cnj(f2:f3)) f1 = Cnj [Dsj[f1', f2'] ,Dsj([f1']++f3')] where
+                                 f1' = distribute f1
+                                 f2' = distribute f2
+                                 f3' = map (\f -> distribute f ) f3
+distributeDsj' f1 f2 = Dsj [f1', f2'] where
+                       f1' = distribute f1
+                       f2' = distribute f2
+
