02fdc5a13b2e52ff46045228eb483ed142fa7654
Alberto Mtnz <me@albertommr.com>
2015-09-21 13:34:26 +0200

    Adds the folder with the files for week-4

---
+++ week-4/Lab4.hs
@@ -0,0 +1,22 @@
+module Lab4 where
+
+import Data.List
+import System.Random
+import Test.QuickCheck
+import SetOrd
+
+-- Exercise 1
+
+-- Exercise 2
+
+-- Exercise 3
+
+-- Exercise 4
+
+-- Exercise 5
+
+-- Exercise 6
+
+-- Exercise 7
+
+-- Exercise 8
