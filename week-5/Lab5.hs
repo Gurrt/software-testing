e22d82b43bb86797b7678390b26958e208614300
Robert Diebels <robertdiebels@grexx.net>
2015-10-03 15:34:00 +0200

    - Added qualified imports so we can use different implementations from the exercises in the same file. - Moved showing nrcGrid into Exercise1.hs - added showNrcGrid

--- week-5/Lab5.hs
+++ week-5/Lab5.hs
@@ -2,11 +2,13 @@ module Lab5 where
 
 import Data.List
 import System.Random
-import Lecture5
+import qualified Lecture5 as L
+import qualified Exercise1 as E1
+import qualified Exercise2 as E2
 
 -- Exercise 1
 
-nrcGrid :: Grid
+nrcGrid :: L.Grid
 nrcGrid = [[0,0,0,3,0,0,0,0,0],
            [0,0,0,7,0,0,3,0,0],
            [2,0,0,0,0,0,0,0,8],
@@ -17,54 +19,8 @@ nrcGrid = [[0,0,0,3,0,0,0,0,0],
            [0,8,0,0,4,0,0,0,0],
            [0,0,2,0,0,0,0,0,0]]
 
-nrcGridLine :: String
-nrcGridLine = "+---------+----------+----------+"
-nrcSubGridLine:: String
-nrcSubGridLine = "|   +-----|---+   +---|-----+   |"
-
-showNrcGrid:: Grid -> IO()
-showNrcGrid [as,bs,cs,ds,es,fs,gs,hs,is] =
-    do putStrLn (nrcGridLine)
-       showNormalRow as;
-       putStrLn (nrcSubGridLine)
-       showNrcSubGridRow bs; showNrcSubGridRow cs
-       putStrLn (nrcGridLine)
-       showNrcSubGridRow ds;
-       putStrLn (nrcSubGridLine)
-       showNormalRow es;
-       putStrLn (nrcSubGridLine)
-       showNrcSubGridRow fs;
-       putStrLn (nrcGridLine)
-       showNrcSubGridRow gs; showNrcSubGridRow hs
-       putStrLn (nrcSubGridLine)
-       showNormalRow is;
-       putStrLn (nrcGridLine)
-
-showNrcSubGridRow:: [Value] -> IO()
-showNrcSubGridRow vs = showNrcRow '|' vs
-
-showNormalRow:: [Value] -> IO()
-showNormalRow vs = showNrcRow ' ' vs
-
-showNrcRow :: Char -> [Value] -> IO()
-showNrcRow ch [a1,a2,a3,a4,a5,a6,a7,a8,a9] =
-   do  putChar '|'         ; putChar ' '
-       putStr (showVal a1) ; putChar ' '
-       putChar ch          ; putChar ' '
-       putStr (showVal a2) ; putChar ' '
-       putStr (showVal a3) ; putChar ' '
-       putChar '|'         ; putChar ' '
-       putStr (showVal a4) ; putChar ' '
-       putChar ch          ; putChar ' '
-       putStr (showVal a5) ; putChar ' '
-       putChar ch          ; putChar ' '
-       putStr (showVal a6) ; putChar ' '
-       putChar '|'         ; putChar ' '
-       putStr (showVal a7) ; putChar ' '
-       putStr (showVal a8) ; putChar ' '
-       putChar ch          ; putChar ' '
-       putStr (showVal a9) ; putChar ' '
-       putChar '|'         ; putChar '\n'
+showNrcGrid:: IO()
+showNrcGrid = E1.showGrid nrcGrid
 
 -- Exercise 2
 
