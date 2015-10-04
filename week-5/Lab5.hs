a4f5480e03050bf05b29a8506352c4bda4d85332
Robert Diebels <robertdiebels@grexx.net>
2015-10-04 16:32:45 +0200

    Added answer to extensibility question and indications of time spent.

--- week-5/Lab5.hs
+++ week-5/Lab5.hs
@@ -7,6 +7,8 @@ import qualified Exercise1 as E1
 import qualified Exercise2 as E2
 
 -- Exercise 1
+-- Time spent: 3 hours
+
 --Grid definitions are the same in all files but for the sake of compatibility E1 is used.
 nrcGrid :: E1.Grid
 nrcGrid = [[0,0,0,3,0,0,0,0,0],
@@ -63,6 +65,18 @@ solveNrcGrid = E1.solveAndShow nrcGrid
 -- +---------+----------+----------+
 
 -- Exercise 2
+-- Time spent: 2 hours
+--
+-- In the chosen solutions the solution of exercise 1 bares striking resemblance to the solution of exercise 2.
+-- However the solution in exercise 2 allows for the removal of several methods making the processing of constraints
+-- uniform.
+-- Injectives still have to be applied to seperate block-definitions as in exercise 1 as the definition of a multitide
+-- of blocks doesn't allow for extensibility.
+--
+-- Conclusion: In the chosen solutions adding more constraints isn't easier or harder. It is however much clearer what
+-- certain functions do, due to increased readability after proposed refactor.
+-- It should be possible to improve the code even further. However a choice was made to go with the solutions that solved
+-- the nrcGrid sudoku and improve upon the code if time would allow us to do so.
 
 showGridE2:: IO()
 showGridE2 = E2.showGrid nrcGrid
