7ccdc91f7e9291efb7037220fb4ed7fc299fd259
Alberto Mtnz <me@albertommr.com>
2015-10-04 20:21:29 +0200

    Defines a property of the number of solutions in an BFS search.

--- week-5/Lab5.hs
+++ week-5/Lab5.hs
@@ -9,6 +9,29 @@ import Lecture5
 -- Exercise 2
 
 -- Exercise 3
+-- Starts 19:00 (+15')
+-- Start at 21:15 (+15')
+-- Start at 20:00
+
+-- A sudoku problem is minimal if
+-- 1) it admits a unique solution
+-- 2) every other sudoku substracting one value accept more than one solution.
+-- We can split this problem into checking both conditions independetly.
+
+bfsSearch :: (node -> [node]) -> (node -> Bool) -> [node] -> [node]
+bfsSearch _ _ [] = []
+bfsSearch children goal (x:xs)
+  | goal x    = x : bfsSearch children goal xs
+  | otherwise = bfsSearch children goal (xs ++ children x)
+
+solveBfsNs :: [Node] -> [Node]
+solveBfsNs = bfsSearch succNode solved
+
+countSolutions :: Grid -> Int
+countSolutions gr =  length (solveBfsNs (initNode gr))
+
+admitsOneSolution :: Grid -> Bool
+admitsOneSolution gr = countSolutions gr == 1
 
 -- Exercise 4
 
