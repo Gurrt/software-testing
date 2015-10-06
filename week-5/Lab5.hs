5f7cd5d96a2de1c378c548afb8cec40ecd384946
CarlaA <camialra11@gmail.com>
2015-10-06 18:28:38 +0200

    Exercise 4
    Changing approach

--- week-5/Lab5.hs
+++ week-5/Lab5.hs
@@ -1,14 +1,15 @@
   module Lab5 where
   
   import Data.List
-  import System.Random
+  import System.Random  
+  import Data.Maybe
+  import Data.Char
 
   type Row    = Int 
   type Column = Int 
   type Value  = Int
   type Grid   = [[Value]]
   type Position = (Row,Column)
-  type Constrnt = [[Position]]
   
   positions, values :: [Int]
   positions = [1..9]
@@ -328,15 +329,15 @@
   eraseN n (r,c) = (s, constraints s) 
     where s = eraseS (fst n) (r,c) 
 
-  --Exercise 4
+  --Exercise 4: time aprox 7 hours (struggling with Haskell).
 
-  delBlockN :: Int->Node->Node
-  delBlockN n node = (s, constraints s)
-    where s = delBlockS n (fst node) 
+  delBlockN :: String ->Node-> Node
+  delBlockN nameB node = (s, constraints s)
+    where s = delBlockS nameB (fst node) 
 
-  delBlockS :: Int -> Sudoku -> Sudoku
-  delBlockS 0 s = s
-  delBlockS n s = foldl delBlock s (take n [(6,6), (1,1), (1,9), (9,1), (9,9)])
+  delBlockS :: String -> Sudoku -> Sudoku
+  delBlockS [] s = s
+  delBlockS nameB s = foldl delBlock s (take (length nameB) (findBlockCoordinates nameB))
 
   delBlock :: Sudoku -> (Row,Column) ->  Sudoku
   delBlock s (r,c) (x,y) = (foldl eraseS s (subBlockPosition s (r,c))) (x,y) 
@@ -344,7 +345,13 @@
   subBlockPosition :: Sudoku -> Position -> [Position]
   subBlockPosition s (r, c) = [(r',c') | r' <- (bl r), c' <- (bl c)]
 
- 
+  findBlockCoordinates:: String -> [(Row, Column)]
+  findBlockCoordinates [] = []
+  findBlockCoordinates (x:xs) = findBlk (toUpper ([x] !!0)) : findBlockCoordinates xs
+
+  findBlk :: Char -> (Row, Column)
+  findBlk c = fromJust (lookup c [('A',(1,1)), ('B',(1,6)), ('C', (1,9)),('D', (6,1)),('E', (6,6)),('F', (6,9)),('G', (9,1)), ('H', (9,6)),('I',(9,9))])
+
   minimalize :: Node -> [(Row,Column)] -> Node
   minimalize n [] = n
   minimalize n ((r,c):rcs) | uniqueSol n' = minimalize n' rcs
@@ -360,14 +367,30 @@
                     return (minimalize n ys)
      where xs = filledPositions (fst n)
 
-  -- delete nB number of blocks nodes not <=5
-  main :: Int -> IO ()
-  main nB = do 
+
+  -- nameB given letter to choose a block nodes 
+
+  main :: String -> IO ()
+  main nameB = do 
           [r] <- rsolveNs [emptyN]
           showNode r
-          let sBlock = delBlockN nB r
+          let sBlock = delBlockN nameB r
           s  <- genProblem sBlock
           showNode s
 
-  --test: main 4
+  --test: main "eI"
+  -- +-------+-------+-------+
+  -- |x      |      x|      x|
+  -- |   A   |   B   |   C   |
+  -- |       |       |       |
+  -- +-------+-------+-------+
+  -- |       |       |       |
+  -- |   D   |   E   |   F   |
+  -- |x      |      x|      x|
+  -- +-------+-------+-------+
+  -- |       |       |       |
+  -- |  G    |   H   |   I   |
+  -- |x      |     x |      x|
+  -- +-------+-------+-------+
+
 
