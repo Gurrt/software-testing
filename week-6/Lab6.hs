b1f43f89e2f4112cfa04a5f9daca2017700091b0
Robert Diebels <robertdiebels@grexx.net>
2015-10-08 20:20:34 +0200

    Implementation of solution found on wikipedia.

--- week-6/Lab6.hs
+++ week-6/Lab6.hs
@@ -4,6 +4,10 @@ import qualified Lecture6 as L
 
 -- Exercise 1
 
+exM :: Integer -> Integer -> Integer -> Integer
+exM b 1 m = rem (b*1) m
+exM b e m = rem (b* (exM b (e-1) m) ) m
+
 -- Exercise 2
 
 -- Exercise 3
