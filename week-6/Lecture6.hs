5dba21062a8de682b30630fe5b5f232af18c5797
Alberto Mtnz <me@albertommr.com>
2015-10-11 15:00:37 +0200

    Cleans warnings.

--- week-6/Lecture6.hs
+++ week-6/Lecture6.hs
@@ -8,32 +8,37 @@
   factors_naive :: Integer -> [Integer]
   factors_naive n = factors' n 2 where 
     factors' 1 _ = []
-    factors' n m 
-      | n `mod` m == 0 = m : factors' (n `div` m) m
-      | otherwise      =     factors' n (m+1)
+    factors' n' m 
+      | n' `mod` m == 0 = m : factors' (n' `div` m) m
+      | otherwise       =     factors' n' (m+1)
 
   factors :: Integer -> [Integer]
   factors n = let 
-     ps = takeWhile (\m -> m^2 <= n) primes
+     ps = takeWhile (\m -> m^(2::Integer) <= n) primes
    in factors' n ps where 
-     factors' 1 _  = []
-     factors' n [] = [n]
-     factors' n (p:ps) 
-      | n `mod` p == 0 = p: factors' (n `div` p) (p:ps)
-      | otherwise      =    factors' n ps
-
+     factors' 1 _   = []
+     factors' n' [] = [n']
+     factors' n' (p:ps) 
+      | n' `mod` p == 0 = p: factors' (n' `div` p) (p:ps)
+      | otherwise       =    factors' n' ps
+  
+  isPrime::Integer->Bool
   isPrime n = factors n == [n]
-  primes = 2 : filter isPrime [3..]
 
-  m1  = 2^2-1;    m2  = 2^3-1;     m3  = 2^5-1
-  m4  = 2^7-1;    m5  = 2^13-1;    m6  = 2^17-1 
-  m7  = 2^19-1;   m8  = 2^31-1;    m9  = 2^61-1
-  m10 = 2^89-1;   m11 = 2^107-1;   m12 = 2^127-1
-  m13 = 2^521-1;  m14 = 2^607-1;   m15 = 2^1279-1
-  m16 = 2^2203-1; m17 = 2^2281-1;  m18 = 2^3217-1
-  m19 = 2^4253-1; m20 = 2^4423-1;  m21 = 2^9689-1
-  m22 = 2^9941-1; m23 = 2^11213-1; m24 = 2^19937-1
-  m25 = 2^21701-1
+  primes::[Integer]
+  primes = 2 : filter isPrime [3..]
+  
+  m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15::Integer
+  m16,m17,m18,m19,m20,m21,m22,m23,m24,m25::Integer
+  m1  = 2^(2::Int)-1;    m2  = 2^(3::Int)-1;     m3  = 2^(5::Int)-1
+  m4  = 2^(7::Int)-1;    m5  = 2^(13::Int)-1;    m6  = 2^(17::Int)-1 
+  m7  = 2^(19::Int)-1;   m8  = 2^(31::Int)-1;    m9  = 2^(61::Int)-1
+  m10 = 2^(89::Int)-1;   m11 = 2^(107::Int)-1;   m12 = 2^(127::Int)-1
+  m13 = 2^(521::Int)-1;  m14 = 2^(607::Int)-1;   m15 = 2^(1279::Int)-1
+  m16 = 2^(2203::Int)-1; m17 = 2^(2281::Int)-1;  m18 = 2^(3217::Int)-1
+  m19 = 2^(4253::Int)-1; m20 = 2^(4423::Int)-1;  m21 = 2^(9689::Int)-1
+  m22 = 2^(9941::Int)-1; m23 = 2^(11213::Int)-1; m24 = 2^(19937::Int)-1
+  m25 = 2^(21701::Int)-1
 
   addM :: Integer -> Integer -> Integer -> Integer
   addM x y = rem (x+y)
@@ -77,7 +82,7 @@
 
   decomp :: Integer -> (Integer,Integer)
   decomp n = decomp' (0,n) where
-    decomp' = until (odd.snd) (\ (m,n) -> (m+1,div n 2))
+    decomp' = until (odd.snd) (\ (m,n') -> (m+1,div n' 2))
 
   primeMR :: Int -> Integer -> IO Bool
   primeMR _ 2 = return True
@@ -99,9 +104,8 @@
   encodeDH :: Integer -> Integer -> Integer -> Integer
   encodeDH p k m = m*k `mod` p
 
-  decodeDH :: Integer -> Integer -> Integer 
-           -> Integer -> Integer -> Integer
-  decodeDH p k ga b c = let 
+  decodeDH :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+  decodeDH p _ ga b c = let 
       gab' = exM ga ((p-1)-b) p 
     in 
       rem (c*gab') p
@@ -129,7 +133,7 @@
 
   rsa_public :: Integer -> Integer -> (Integer,Integer)
   rsa_public p q = let 
-     n   = p * q
+     -- n   = p * q
      phi = (p-1)*(q-1)
      e   = head [ x | x <- [3..], gcd x phi == 1 ]
    in 
@@ -138,7 +142,7 @@
   rsa_private ::  Integer -> Integer 
                   -> (Integer,Integer)
   rsa_private p q = let 
-     n = p * q
+     -- n = p * q
      phi = (p-1)*(q-1)
      e = head [ x | x <- [3..], gcd x phi == 1 ]
      d = invM e phi 
@@ -148,11 +152,14 @@
   rsa_encode :: (Integer,Integer) -> Integer -> Integer 
   rsa_encode (e,n) =  \ m -> exM m e n
 
+  rsa_decode:: (Integer, Integer) -> Integer -> Integer
   rsa_decode = rsa_encode
 
   trapdoor :: (Integer,Integer) -> Integer -> Integer
   trapdoor = rsa_encode 
-
+  
+  secret::Integer
   secret = m18
+  
+  bound::Integer
   bound  = 131
-
