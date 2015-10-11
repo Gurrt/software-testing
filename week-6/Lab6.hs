a7509810316a7a7e38a5f973e1fe96115747defd
Alberto Mtnz <me@albertommr.com>
2015-10-11 18:46:01 +0200

    Sets the exercise 5

--- week-6/Lab6.hs
+++ week-6/Lab6.hs
@@ -11,7 +11,12 @@ import qualified Lecture6 as L
 -- Exercise 4
 
 -- Exercise 5
-
+carmichael :: [Integer]
+carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) | 
+        k <- [2..], 
+        L.isPrime (6*k+1), 
+        L.isPrime (12*k+1), 
+        L.isPrime (18*k+1) ]
 -- Exercise 6
 
 -- Exercise 7
