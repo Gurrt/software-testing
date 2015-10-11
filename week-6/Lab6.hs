c5328be0b5e8d11b6bcfd4257c105ab392b9e4f1
Alberto Mtnz <me@albertommr.com>
2015-10-11 23:49:12 +0200

    Working on solution for exercise 5

--- week-6/Lab6.hs
+++ week-6/Lab6.hs
@@ -11,12 +11,30 @@ import qualified Lecture6 as L
 -- Exercise 4
 
 -- Exercise 5
+
+-- In order to
 carmichael :: [Integer]
-carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) | 
-        k <- [2..], 
-        L.isPrime (6*k+1), 
-        L.isPrime (12*k+1), 
+carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) |
+        k <- [2..],
+        L.isPrime (6*k+1),
+        L.isPrime (12*k+1),
         L.isPrime (18*k+1) ]
+
+-- Test the first k Carmichael numbers using the Fermat's primalty test.
+testFermatC :: Int -> IO [Bool]
+testFermatC k = mapM L.prime_test_F (take k carmichael)
+
+printTest :: IO Bool -> IO()
+printTest b = do
+                b' <- b
+                if b' then print ("True"::String)
+                    else print ("False"::String)
+
+testEx5 :: Int -> [IO()]
+testEx5 k = do
+              arr <- testFermatC k
+              print arr
+
 -- Exercise 6
 
 -- Exercise 7
--- week-6/Lecture6.hs
+++ week-6/Lecture6.hs
@@ -1,37 +1,37 @@
   module Lecture6
-  
-  where 
-  
+
+  where
+
   import Data.List
   import System.Random
 
-  factors_naive :: Integer -> [Integer]
-  factors_naive n = factors' n 2 where 
+--  {-# ANN module "HLint: Use camelCase Found:" #-}
+  factosNaive :: Integer -> [Integer]
+  factosNaive n = factors' n 2 where
     factors' 1 _ = []
-    factors' n' m 
+    factors' n' m
       | n' `mod` m == 0 = m : factors' (n' `div` m) m
       | otherwise       =     factors' n' (m+1)
 
   factors :: Integer -> [Integer]
-  factors n = let 
-     ps = takeWhile (\m -> m^(2::Integer) <= n) primes
-   in factors' n ps where 
-     factors' 1 _   = []
-     factors' n' [] = [n']
-     factors' n' (p:ps) 
-      | n' `mod` p == 0 = p: factors' (n' `div` p) (p:ps)
-      | otherwise       =    factors' n' ps
-  
+  factors n = let ps = takeWhile (\m -> m^(2::Integer) <= n) primes
+               in factors' n ps where
+                 factors' 1 _   = []
+                 factors' n' [] = [n']
+                 factors' n' (p:ps)
+                  | n' `mod` p == 0 = p: factors' (n' `div` p) (p:ps)
+                  | otherwise       =    factors' n' ps
+
   isPrime::Integer->Bool
   isPrime n = factors n == [n]
 
   primes::[Integer]
   primes = 2 : filter isPrime [3..]
-  
+
   m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15::Integer
   m16,m17,m18,m19,m20,m21,m22,m23,m24,m25::Integer
   m1  = 2^(2::Int)-1;    m2  = 2^(3::Int)-1;     m3  = 2^(5::Int)-1
-  m4  = 2^(7::Int)-1;    m5  = 2^(13::Int)-1;    m6  = 2^(17::Int)-1 
+  m4  = 2^(7::Int)-1;    m5  = 2^(13::Int)-1;    m6  = 2^(17::Int)-1
   m7  = 2^(19::Int)-1;   m8  = 2^(31::Int)-1;    m9  = 2^(61::Int)-1
   m10 = 2^(89::Int)-1;   m11 = 2^(107::Int)-1;   m12 = 2^(127::Int)-1
   m13 = 2^(521::Int)-1;  m14 = 2^(607::Int)-1;   m15 = 2^(1279::Int)-1
@@ -44,24 +44,24 @@
   addM x y = rem (x+y)
 
   multM :: Integer -> Integer -> Integer -> Integer
-  multM x y = rem (x*y) 
+  multM x y = rem (x*y)
 
   invM :: Integer -> Integer -> Integer
-  invM x n = let 
-     (u,v) = fct_gcd x n
+  invM x n = let
+     (u,v) = fctGcd x n
      copr  = x*u + v*n == 1
-     i     = if signum u == 1 then u else u + n  
-   in 
+     i     = if signum u == 1 then u else u + n
+   in
      if copr then i else error "no inverse"
 
-  fct_gcd :: Integer -> Integer -> (Integer,Integer) 
-  fct_gcd a b = 
-    if b == 0 
-    then (1,0) 
-    else 
-       let 
+  fctGcd :: Integer -> Integer -> (Integer,Integer)
+  fctGcd a b =
+    if b == 0
+    then (1,0)
+    else
+       let
          (q,r) = quotRem a b
-         (s,t) = fct_gcd b r 
+         (s,t) = fctGcd b r
        in (t, s - q*t)
 
   expM ::  Integer -> Integer -> Integer -> Integer
@@ -71,7 +71,7 @@
   exM = expM -- to be replaced by a fast version
 
   prime_test_F :: Integer -> IO Bool
-  prime_test_F n = do 
+  prime_test_F n = do
      a <- randomRIO (1, n-1) :: IO Integer
      return (exM a (n-1) n == 1)
 
@@ -87,17 +87,16 @@
   primeMR :: Int -> Integer -> IO Bool
   primeMR _ 2 = return True
   primeMR 0 _ = return True
-  primeMR k n = let 
-     (r,s) = decomp (n-1) 
-     f = \ x -> takeWhile (/= 1) 
-         (map (\ j -> exM x (2^j*s) n)  [0..r])
-    in 
-     do 
+  primeMR k n = let
+     (r,s) = decomp (n-1)
+     f = \ x -> takeWhile (/= 1) (map (\ j -> exM x (2^j*s) n)  [0..r])
+    in
+     do
       a <- randomRIO (1, n-1) :: IO Integer
-      if exM a (n-1) n /= 1 
-        then return False 
-        else 
-          if exM a s n /= 1 && last (f a) /= (n-1) 
+      if exM a (n-1) n /= 1
+        then return False
+        else
+          if exM a s n /= 1 && last (f a) /= (n-1)
             then return False
             else primeMR (k-1) n
 
@@ -105,61 +104,61 @@
   encodeDH p k m = m*k `mod` p
 
   decodeDH :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
-  decodeDH p _ ga b c = let 
-      gab' = exM ga ((p-1)-b) p 
-    in 
+  decodeDH p _ ga b c = let
+      gab' = exM ga ((p-1)-b) p
+    in
       rem (c*gab') p
 
   encode :: Integer -> Integer -> Integer -> Integer
-  encode p k m = let 
+  encode p k m = let
      p' = p-1
      e  = head [ x | x <- [k..], gcd x p' == 1 ]
-   in 
+   in
      exM m e p
-  
+
   decode :: Integer -> Integer -> Integer -> Integer
-  decode p k m = let 
+  decode p k m = let
      p' = p-1
      e  = head [ x | x <- [k..], gcd x p' == 1 ]
-     d  = invM e p' 
-   in 
+     d  = invM e p'
+   in
      exM m d p
 
   cipher :: Integer -> Integer
   cipher = encode secret bound
-  
+
   decipher :: Integer -> Integer
   decipher = decode secret bound
 
   rsa_public :: Integer -> Integer -> (Integer,Integer)
-  rsa_public p q = let 
+  rsa_public p q = let
      -- n   = p * q
      phi = (p-1)*(q-1)
      e   = head [ x | x <- [3..], gcd x phi == 1 ]
-   in 
+   in
      (e,p*q)
 
-  rsa_private ::  Integer -> Integer 
+  rsa_private ::  Integer -> Integer
                   -> (Integer,Integer)
-  rsa_private p q = let 
+  rsa_private p q = let
      -- n = p * q
      phi = (p-1)*(q-1)
      e = head [ x | x <- [3..], gcd x phi == 1 ]
-     d = invM e phi 
-    in 
+     d = invM e phi
+    in
      (d,p*q)
 
-  rsa_encode :: (Integer,Integer) -> Integer -> Integer 
+  rsa_encode :: (Integer,Integer) -> Integer -> Integer
   rsa_encode (e,n) =  \ m -> exM m e n
 
   rsa_decode:: (Integer, Integer) -> Integer -> Integer
   rsa_decode = rsa_encode
 
   trapdoor :: (Integer,Integer) -> Integer -> Integer
-  trapdoor = rsa_encode 
-  
+  trapdoor = rsa_encode
+
   secret::Integer
   secret = m18
-  
+
   bound::Integer
   bound  = 131
